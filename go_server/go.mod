f9dc854fbb494092a47231919b9df15b72d4fca9
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-06-02 20:57:41 +0530

    added cors to connect to react server

--- go_server/go.mod
+++ go_server/go.mod
@@ -7,3 +7,5 @@ require (
 	// github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/gorilla/mux v1.8.0
 )
+
+require github.com/rs/cors v1.9.0
--- go_server/routes/routes.go
+++ go_server/routes/routes.go
@@ -8,12 +8,23 @@ import (
 	"github.com/DeepjyotiSarmah/go_server/middlewares"
 
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func SetupRoutes() {
 	r := mux.NewRouter()
 
-	//create routes
+	// Create a new CORS handler with desired options
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://127.0.0.1:5173"}, // Replace with your frontend domain
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders: []string{"Content-Type", "Authorization"},
+	})
+
+	// Apply the CORS middleware to your router
+	r.Use(corsHandler.Handler)
+
+	// Create routes...
 	r.HandleFunc("/", controllers.GetHome).Methods("GET")
 	r.HandleFunc("/problems", controllers.GetProblems).Methods("GET")
 	r.HandleFunc("/problems/{id}", controllers.GetProblemById).Methods("GET")
@@ -23,7 +34,6 @@ func SetupRoutes() {
 	r.HandleFunc("/signup", controllers.SignUp).Methods("POST")
 	r.HandleFunc("/login", controllers.Login).Methods("POST")
 
-	// //start server
+	// Start server
 	log.Fatal(http.ListenAndServe(":3000", r))
-
 }
